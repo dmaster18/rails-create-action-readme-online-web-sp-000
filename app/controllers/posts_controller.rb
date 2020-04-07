d4304ca0214307b93a1187e6c79a5e9dff25eeae
David Strauch <dmaster18@gmail.com>
2020-04-07 21:28:57 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,8 +14,8 @@ class PostsController < ApplicationController
   def create
     @post = Post.new
     @post.title = params[:title]
-    post.description = params[:description]
-    post.save
+    @post.description = params[:description]
+    @post.save
   end
   
 end
